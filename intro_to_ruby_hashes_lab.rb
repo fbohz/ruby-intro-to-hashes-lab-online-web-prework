6151e9f3aa5dadf1a243f9960a7f5cf01bb51b5a
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-28 16:49:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,10 @@ def monopoly_with_third_tier
 	    names: {
 	      reading_railroad: {
 	        
-	      }
+	      },
+	      pennsylvania_railroad: {
+	        
+	      },
 	      
 	    },
 	    rent_in_dollars: {
