7728903ce57e782797290e57ab6a7c18b2b5c979
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-28 16:47:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,7 +43,9 @@ def monopoly_with_third_tier
 	      
 	    },
 	    rent_in_dollars: {
-	      
+	      one_piece_owned: 25,
+	      two_piece_owned: 50,
+	      one_piece_owned
 	    }
 	   
 	  }
